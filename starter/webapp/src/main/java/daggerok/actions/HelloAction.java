3a2d6a9547ba044b2c0ca1cbc07e591150161f63
Maksim Kostromin <daggerok@gmail.com>
2018-01-08 08:42:29 +0200

    update starter for messages file

--- starter/webapp/src/main/java/daggerok/actions/HelloAction.java
+++ starter/webapp/src/main/java/daggerok/actions/HelloAction.java
@@ -1,21 +1,25 @@
 package daggerok.actions;
 
-import com.opensymphony.xwork2.Action;
-import lombok.Data;
+import com.opensymphony.xwork2.ActionSupport;
 import lombok.extern.log4j.Log4j2;
+import lombok.val;
 
 import java.util.Date;
 
-@Data
 @Log4j2
-public class HelloAction implements Action {
+public class HelloAction extends ActionSupport {
 
-  String message;
+  private static final long serialVersionUID = -1401461151989527781L;
 
   @Override
   public String execute() throws Exception {
     log.info("executed at: {}", new Date());
-    setMessage("ololo-trololo!");
     return "success";
+  }
+
+  public String getMessage() {
+    val text = getText("hello.message");
+    log.info("parsed from properties file: {}", text);
+    return text;
   }
 }
